fa281cba93074aaa403ccd5f224200468a0cfd5c
Hector2721 <100887293+Hector2721@users.noreply.github.com>
2022-03-09 01:08:27 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,11 @@
+if(navigator.serviceWorker) {
+
+    navigator.serviceWorker.register("/sw.js");
+
+    console.log("Si jala");
+
+} else {
+
+    console.log("Aquí no jala el service worker!!!")
+
+}
